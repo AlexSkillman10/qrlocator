28c6adbd621b14e00eb85ffbceed83820fe54d2c
AlexSkillman10 <alexskillman10@gmail.com>
2023-09-25 11:35:32 -0500

    added adaptive calibration

--- qr_location_calculator/qrcalibrator.py
+++ qr_location_calculator/qrcalibrator.py
@@ -1,4 +1,5 @@
 import os
+import time
 import cv2
 from qrlocator import QRLocator
 
@@ -13,7 +14,9 @@ def calibrate_qr_locator(calibration_images_folder):
     best_error = float('inf')
     best_focal_length = None
     best_sensor_width = None
-
+    best_focal_angle_scalar = None
+    best_qr_code_details = []
+    
     calibration_images = []
 
     for image_name in os.listdir(calibration_images_folder):
@@ -22,45 +25,82 @@ def calibrate_qr_locator(calibration_images_folder):
 
         image_path = os.path.join(calibration_images_folder, image_name)
         calibration_images.append(
-            [image_name, QRLocator(image_path, 1, 1, QR_CODE_SIZE_INCHES)])
+            [image_name, QRLocator(image_path, 1, 1, 1, QR_CODE_SIZE_INCHES)])
+        
+    step_counts = [20, 10, 5, 2.5, 1, .5, .1, .05, .01]
+    
+    best_focal_length = 51
+    best_sensor_width = 51
+    best_focal_angle_scalar = 1.1
 
-    for focal_length in np.arange(20, 30, 0.1):
-        for sensor_width in np.arange(30, 40, 0.1):
 
-            total_error = 0
+    for i in range(len(step_counts)):
 
-            for calibration_section in calibration_images:
+        step_count = step_counts[i]
+        prev_step_count = step_counts[i-1] if i > 0 else 50
+        prev_step_count = prev_step_count*1.1 if prev_step_count*1.1 < 50 else prev_step_count
 
-                image_name = calibration_section[0]
-                qr_locator = calibration_section[1]
+        best_focal_length_loop = best_focal_length
+        best_sensor_width_loop = best_sensor_width
+        best_focal_angle_scalar_loop = best_focal_angle_scalar
 
-                qr_locator.focal_length_mm = focal_length
-                qr_locator.sensor_width_mm = sensor_width
-                qr_locator.process_codes()
 
-                real_y = int(image_name.replace('ft.jpg', '')) * 12
+        for focal_length in np.arange(best_focal_length_loop-prev_step_count, best_focal_length_loop+prev_step_count, step_count):
+            for sensor_width in np.arange(best_sensor_width_loop-prev_step_count, best_sensor_width_loop+prev_step_count, step_count):
+                for focal_angle_scalar in np.arange(best_focal_angle_scalar_loop-prev_step_count/50, best_focal_angle_scalar_loop+prev_step_count/50, step_count/50):
 
-                for data, qr_code in qr_locator.codes.items():
-                    known_y = real_y
-                    error = abs(qr_code.y - known_y)
-                    total_error += error
-                    # print(f"QR Code: {data}, Error: {error}, Y: {qr_code.y}, Known Y: {known_y}")
+                    total_error = 0
+                    qr_code_details = []
 
-            if total_error < best_error:
-                best_error = total_error
-                best_focal_length = focal_length
-                best_sensor_width = sensor_width
+                    for calibration_section in calibration_images:
 
-            print(
-                f"Focal Length: {focal_length}, Sensor Width: {sensor_width}, Total Error: {total_error}")
+                        image_name = calibration_section[0]
+                        qr_locator = calibration_section[1]
+
+                        qr_locator.focal_length_mm = focal_length
+                        qr_locator.sensor_width_mm = sensor_width
+                        qr_locator.focal_angle_scalar = focal_angle_scalar
+                        qr_locator.process_codes()
+
+                        real_y = int(image_name.replace('ft.jpg', '')) * 12
+
+                        qr_code_details.append(f"{image_name}")
+
+                        for data, qr_code in qr_locator.codes.items():
+                            known_y = real_y
+                            known_x = calibration_data[data]['x']
+                            error = abs(qr_code.y - known_y) + abs(qr_code.x - known_x)
+                            total_error += error
+                            qr_code_details.append(
+                                f"QR Code: {data}, Error: {error}, Y: {qr_code.y}, Known Y: {known_y}, X: {qr_code.x}, Known X: {known_x}")
+                    
+                    if total_error < best_error:
+                        best_error = total_error
+                        best_focal_length = focal_length
+                        best_sensor_width = sensor_width
+                        best_focal_angle_scalar = focal_angle_scalar
+                        best_qr_code_details = qr_code_details
+
+                    print(f"Focal Length: {focal_length}, Sensor Width: {sensor_width}, Focal Angle Scalar: {focal_angle_scalar}, Total Error: {total_error}")
 
     with open('calibration_results.txt', 'w') as file:
+        file.write(f"Best Focal Ratio: {best_focal_length / best_sensor_width}\n")
+        print(f"Best Focal Ratio: {best_focal_length / best_sensor_width}")
+        print(f"Best Focal Ratio: {best_sensor_width / best_focal_length}")
         file.write(f"Best Focal Length: {best_focal_length}\n")
         print(f"Best Focal Length: {best_focal_length}")
         file.write(f"Best Sensor Width: {best_sensor_width}\n")
         print(f"Best Sensor Width: {best_sensor_width}")
+        file.write(f"Best Focal Angle Scalar: {best_focal_angle_scalar}\n")
+        print(f"Best Focal Angle Scalar: {best_focal_angle_scalar}")
         file.write(f"Minimum Total Error: {best_error}\n")
         print(f"Minimum Total Error: {best_error}")
+        
+        # Write the best QR code details to the file
+        file.write("Best QR Code Details:\n")
+        for detail in best_qr_code_details:
+            file.write(detail + '\n')
+            print(detail)
 
 
 if __name__ == "__main__":
--- qr_location_calculator/qrlocator.py
+++ qr_location_calculator/qrlocator.py
@@ -9,10 +9,10 @@ def convert_focal_length_to_pixels(focal_length_mm, image_width_pixels, sensor_w
 def calculate_distance_to_camera(known_width, focal_length, per_width):
     return (known_width * focal_length) / per_width
 
-def calculate_angle_and_map_2d(centroid_x, distance_inches, img_center, image_width_pixels):
+def calculate_angle_and_map_2d(centroid_x, distance_inches, focal_angle_scalar, img_center, image_width_pixels):
     delta_x = centroid_x - img_center
     angle = math.degrees(math.atan(delta_x / image_width_pixels))
-    x = math.tan(math.radians(angle)) * distance_inches
+    x = math.tan(math.radians(angle)) * distance_inches * focal_angle_scalar
     y = distance_inches
     return angle, x, y
 
@@ -24,7 +24,7 @@ class QRCode:
         self.y = y
 
 class QRLocator:
-    def __init__(self, image_path, focal_length_mm, sensor_width_mm, qr_code_size_inches):
+    def __init__(self, image_path, focal_length_mm, sensor_width_mm, focal_angle_scalar, qr_code_size_inches):
         self.image_path = image_path
         self.image = cv2.imread(image_path)
         
@@ -34,6 +34,7 @@ class QRLocator:
         self.decoded_objects = decode(self.image)
         self.focal_length_mm = focal_length_mm
         self.sensor_width_mm = sensor_width_mm
+        self.focal_angle_scalar = focal_angle_scalar
         self.qr_code_size_inches = qr_code_size_inches
         self.qr_code_size_in_mm = qr_code_size_inches * 25.4
         self.img_center = self.image.shape[1] / 2
@@ -51,7 +52,7 @@ class QRLocator:
             per_width = cv2.norm(points[0], points[1])
             distance_mm = calculate_distance_to_camera(self.qr_code_size_in_mm, focal_length_pixels, per_width)
             distance_inches = distance_mm / 25.4
-            angle, x, y = calculate_angle_and_map_2d(centroid_x, distance_inches, self.img_center, self.image.shape[1])
+            angle, x, y = calculate_angle_and_map_2d(centroid_x, distance_inches, self.focal_angle_scalar, self.img_center, self.image.shape[1])
             qr_code_obj = QRCode(qr_code.data.decode('utf-8'), angle, x, y)
             self.codes[qr_code_obj.data] = qr_code_obj
             
@@ -75,14 +76,20 @@ class QRLocator:
 
 
 if __name__ == "__main__":
-    FOCAL_LENGTH_MM = 25.8
-    SENSOR_WIDTH_MM = 33.3
-    QR_CODE_SIZE_INCHES = 7.93
-    # QR_CODE_SIZE_INCHES = 5.35
-    # IMAGE_PATH = r'calibration\test_images\IMG_0882.jpg'
-    IMAGE_PATH = r'calibration\dev_calibration_images\6ft.jpg'
+    FOCAL_LENGTH_MM = 26.500000000000007
+    SENSOR_WIDTH_MM = 34.20000000000003
+
+    FOCAL_RATIO = FOCAL_LENGTH_MM / SENSOR_WIDTH_MM
     
-    qr_locator = QRLocator(IMAGE_PATH, FOCAL_LENGTH_MM, SENSOR_WIDTH_MM, QR_CODE_SIZE_INCHES)
+    BEST_FOCAL_SCALAR = 1.3000000000000003
+
+    # QR_CODE_SIZE_INCHES = 5.35
+    # IMAGE_PATH = r'calibration\test_images\IMG_0884.jpg'
+
+    QR_CODE_SIZE_INCHES = 7.93
+    IMAGE_PATH = r'calibration\dev_calibration_images\8ft.jpg'
+    
+    qr_locator = QRLocator(IMAGE_PATH, FOCAL_LENGTH_MM, SENSOR_WIDTH_MM, BEST_FOCAL_SCALAR, QR_CODE_SIZE_INCHES)
     qr_locator.show_visualization()
 
 
